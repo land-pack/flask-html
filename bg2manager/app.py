e71a034e3de0a29bfd036f3fe4cfd55ed1074d6e
land-pack <landpack@sina.com>
2017-12-05 10:46:13 +0800

    add more

--- bg2manager/app.py
+++ bg2manager/app.py
@@ -1,9 +1,10 @@
+import os
 from flask import Flask, render_template 
 from flask import request
 from pymongo import MongoClient
 
-
-client = MongoClient('mongodb://crazy_bet_rw:crazy_bet_rw@10.0.1.191:27017/crazy_bet')
+url = os.getenv("OFFLINE_MONGODB")
+client = MongoClient(url)
 db = client.crazy_bet
 
 c  = db.t_backpack_template
@@ -17,18 +18,16 @@ def index():
 	return render_template("index.html")
 
 
+def to_dict(obj):
+    return {k:v for k, v in obj.items() }
+
 @app.route("/input", methods=["POST", "GET"])
 def input():
-    ret = c.find()
-    for i in ret:
-        print i
     if request.method == 'POST':
         data = request.values
-        for k, v in data.items():
-            print 'key ==>', k, 'value ==>', v 
-        recharge_conf = data.get("recharge_conf")
-        print 'recharge_conf', recharge_conf
-        #TODO save it on mongodb
+        data = to_dict(data)
+        print 'data ==>', data
+        c.insert(data)
     return render_template("input.html")
 
 if __name__ == '__main__':
