b9dcdd251adba7ef6606c9c78cecbf21e916c0ae
land-pack <landpack@sina.com>
2017-12-05 15:12:14 +0800

    add list page

--- bg2manager/app.py
+++ bg2manager/app.py
@@ -1,4 +1,5 @@
 import os
+import datetime
 from flask import Flask, render_template 
 from flask import request
 from pymongo import MongoClient
@@ -27,8 +28,28 @@ def input():
         data = request.values
         data = to_dict(data)
         print 'data ==>', data
+        data.update({
+            "created": datetime.datetime.now()
+        })
         c.insert(data)
     return render_template("input.html")
+
+
+@app.route("/list")
+def list_page():
+    lst = []
+    ret = c.find()
+    data = [
+        {
+            "index":i.get("_id", 'ss'),
+            "name": i.get("prize_name", 'ss'),
+            "type": i.get("welfare_type", 'xx'),
+            "created":i.get("created", "2019-9-")
+        }
+        for i in ret
+    ]
+    return render_template("list.html", data=data)
+
 
 if __name__ == '__main__':
 	app.run(debug=True)
